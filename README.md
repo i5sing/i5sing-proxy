1e23fee3a9b55be0586a42da397587b281029308
miaowing <me@zf.ink>
2019-07-24 18:38:27 +0800

    support proxy stream api

--- README.md
+++ README.md
@@ -4,10 +4,19 @@ Provides 5sing HTTPS API for Wechat APP.
 
 ## Usage
 
+JSON:
 ```diff
 // source api:
 - http://mobileapi.5sing.kugou.com/go/GetSongListSquareRecommended?index=1&version=6.8.20
 
 // proxy api:
 + https://proxy.i5sing.com/go/GetSongListSquareRecommended?index=1&version=6.8.20
+```
+
+MP3:
+```diff
+// source api
+- http://wsaudio.bssdlbig.kugou.com/1907241806/utoz19yvtyAmi9msb4libw/1564049171/bss/extname/wsaudio/163c9a68227567ac7ac315ad40e0b1df.mp3
+
++ https://proxy.i5sing.com/stream?url=http:%2F%2Fwsaudio.bssdlbig.kugou.com%2F1907241806%2Futoz19yvtyAmi9msb4libw%2F1564049171%2Fbss%2Fextname%2Fwsaudio%2F163c9a68227567ac7ac315ad40e0b1df.mp3
 ```
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { All, Controller, Param, Req, Res } from '@nestjs/common';
+import { All, BadRequestException, Controller, Param, Query, Req, Res } from '@nestjs/common';
 import { InjectProxy, Proxy } from '@nestcloud/proxy';
+import * as http from 'request';
 
 @Controller('/:service')
 export class AppController {
@@ -9,7 +10,21 @@ export class AppController {
   }
 
   @All()
-  async getHello(@Param('service') service: string, @Req() request, @Res() response): Promise<void> {
+  async getHello(@Param('service') service: string, @Req() request, @Res() response, @Query('url') url: string): Promise<void> {
+    if (service === 'stream') {
+      if (url) {
+        const stream = http.get(decodeURIComponent(url), {
+          headers: {
+            'User-Agent': '5sing%E5%8E%9F%E5%88%9B%E9%9F%B3%E4%B9%90/6081002 CFNetwork/978.0.7 Darwin/18.5.0',
+            'Accept-Encoding': 'gzip,deflate',
+          },
+        });
+        return stream.pipe(response);
+      } else {
+        throw new BadRequestException('Please set querystring parameter url');
+      }
+    }
+
     await this.proxy.forward(request, response, service);
   }
 }
